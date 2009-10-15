f3e5496cc0570a0fab611925aeb012643c548e87
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-15 16:10:31 +0300

    fixed bug with url_conf, erased manage.py

--- main_site/settings.py
+++ main_site/settings.py
@@ -23,6 +23,8 @@ ADMIN_MEDIA_PREFIX = '/media/admin/'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'rndx7w-jze@1$fu!&7x*b*mm&c!)!$5-@gvw^5^u_ixohmiipi'
 
+ROOT_URLCONF = 'main_site.urls'
+
 INSTALLED_APPS += (
     'django.contrib.admin',
     'django.contrib.admindocs',
