e49783ea16cfa4da0138a848acd3f09e7d2a4104
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-20 17:21:16 +0300

    chaged media linking

--- main_site/settings.py
+++ main_site/settings.py
@@ -1,14 +1,6 @@
 from saaskit.settings import *
 
-import os.path
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 SITE_ID = 1
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
-APP_MEDIA_ROOT = MEDIA_ROOT
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'rndx7w-jze@1$fu!&7x*b*mm&c!)!$5-@gvw^5^u_ixohmiipi'
@@ -19,9 +11,4 @@ INSTALLED_APPS += (
     'django.contrib.admin',
     'django.contrib.admindocs',
 )
-
-try:
-    from settings_local import *
-except ImportError:
-    pass
                 
