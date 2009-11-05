bc41a12e5def10713ebf4764d448d7e6ab189e65
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-11-05 11:40:50 +0200

    splitted url mapping

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,33 +1,23 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 
-handler404 = 'perfect404.views.page_not_found'
-
 from django.contrib import admin
 admin.autodiscover()
+
+from saaskit.urls import handler404, urlpatterns as saaskit_patterns
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', dict(template='index.html')),
     (r'^about/$', 'django.views.generic.simple.direct_to_template', dict(template='about.html')),
     (r'^support/$', 'django.views.generic.simple.direct_to_template', dict(template='support.html')),
     (r'^privacy/$', 'django.views.generic.simple.direct_to_template', dict(template='tc.html')),
-    url(r'^sso/$', 'sso.views.sso', name="sso"),
-    (r'^accounts/', include('django_authopenid.urls')),
     (r'^accounts/create-site/$', 'muaccounts.views.create_account'),
-    (r'^profiles/', include('saaskit_profile.urls')),
-    url(r'^subscription/(?P<object_id>\d+)/$', 'subscription.views.subscription_detail', 
-     {'payment_method':'pro' if settings.PAYPAL_PRO else 'standard'}, name='subscription_detail'),
-    (r'^subscription/', include('subscription.urls')),
-    (r'^dashboard/', include('notification.urls')),
     (r'^contact/', include('contact_form.urls')),
     (r'^admin/templatesadmin/', include('templatesadmin.urls')),
     (r'^admin/rosetta/', include('rosetta.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', include(admin.site.urls)),
 )
 
-# serve static files in debug mode
-if settings.SERVE_MEDIA:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
+#apply saaskit-core url mapping
+urlpatterns += saaskit_patterns
