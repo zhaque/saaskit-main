174b9caaeb438b9ac8b653420e18c2a6d72c58eb
SaaSkit <saaskit@DevBook.home>
2009-12-22 16:09:12 +0000

    package re-name

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name="saaskit-main-site",
            version="0.1",
            description="Django project for main site of saaskit",
-           author="CrowdSense",
-           author_email="admin@crowdsense.com",
+           author="SaaS Kit",
+           author_email="admin@saaskit.org",
            packages=find_packages(),
            include_package_data=True,
 )
