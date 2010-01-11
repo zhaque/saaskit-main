a39edf929c016c4c556f04b46504bb50fc78f8ef
CrowdSense <admin@DevBook.home>
2010-01-11 07:16:46 +0000

    fixed naming convention

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name="saaskit-main-site",
            version="0.1",
            description="Django project for main site of saaskit",
-           author="SaaS Kit",
+           author="SaaSKit",
            author_email="admin@saaskit.org",
            packages=find_packages(),
            include_package_data=True,
